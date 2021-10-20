a595dfe43e117236b314ff871000620c1e6ff4d9
Chiatiah Calson <c.chiatiah@dreamonkey.com>
2021-10-20 10:50:27 +0100

    feat(mgmt): add styling for header and homepage

--- docs/src/router/routes.js
+++ docs/src/router/routes.js
@@ -88,6 +88,17 @@ const routes = [
     children: docsPages
   },
 
+  {
+    path: '/home',
+    component: () => import('layouts/MainLayout'),
+    children: [
+      {
+        path: '',
+        component: () => import('pages/landing-page/Index')
+      }
+    ]
+  },
+
   // externals
   {
     path: '/layout-builder',
