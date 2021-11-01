179272e53522a969a77d5183418d1f8143f2fd9f
Chiatiah Calson <c.chiatiah@dreamonkey.com>
2021-11-01 12:42:43 +0100

    feat(docs): add component to embed tweets from twitter

---
+++ docs/src/components/landing-page/TwitterCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div v-html="cardContent">
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { fabTwitter } from '@quasar/extras/fontawesome-v5'
+
+export default defineComponent({
+  name: 'ShowCaseCard',
+  props: {
+    cardContent: {
+      required: true,
+      type: String
+    }
+  },
+  setup () {
+    const scriptElement = document.createElement('script')
+    scriptElement.setAttribute('src', 'https://platform.twitter.com/widgets.js')
+    scriptElement.setAttribute('charset', 'utf-8')
+    document.head.appendChild(scriptElement)
+
+    return {
+      fabTwitter
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+.twitter-tweet {
+  box-shadow: 0 8px 12px 0 rgba($lp-primary, 0.4);
+  border-radius: 10px;
+  overflow: hidden;
+  background-color: $white;
+  max-width: 100%;
+}
+</style>
