9450dd55eaa579534e7d3b6cadb8fde116b83e8e
Chiatiah Calson <c.chiatiah@dreamonkey.com>
2021-11-11 15:40:49 +0100

    refactor(docs): remove unnecessary attributes and elements, simplify html and styles

--- docs/src/pages/landing-page/QuasarComponents.vue
+++ docs/src/pages/landing-page/QuasarComponents.vue
@@ -1,35 +1,29 @@
 <template>
-  <q-page class="column q-mt-xl lp-mb--large text-white q-mx-xl">
-    <div class="row justify-center q-mb-lg">
-      <div class="input-btn">
-        <q-input
-          color="white"
-          input-class="search-field"
-          label-color="grey-6"
-          borderless
-          v-model="search"
-          label="Search component"
-          dense
-          width="xl"
-          class="text-white"
-        >
-          <template #append>
-            <q-icon name="search" size="sm" color="lp-primary" />
-          </template>
-        </q-input>
-      </div>
-    </div>
-    <div class="row justify-center q-mb-md">
+  <q-page class="column items-center q-mt-xl lp-mb--large text-white q-mx-xl">
+    <q-input
+      label-color="grey-6"
+      borderless
+      v-model="search"
+      label="Search component"
+      dense
+      dark
+      class="relative-position search-field q-mb-lg"
+    >
+      <template #append>
+        <q-icon name="search" size="sm" color="lp-primary" />
+      </template>
+    </q-input>
+    <div class="q-mb-md">
       <q-chip v-for="(filterChip, i) in filterChips" :key="i" color="lp-primary" clickable text-color="white" :label="filterChip"/>
     </div>
-    <div class="row components">
-      <q-card v-for="({name, text, imgName}, i) in components" :key="name + i" class="card text-size-16 shadow-bottom-small">
-        <img :src="'components-thumbnails/' + imgName" />
-        <q-card-section>
-          <div class="card__title text-lp-primary text-weight-medium">{{ name }}</div>
+    <div class="components">
+      <q-card v-for="({name, description}, i) in components" :key="name + i" class="raise-on-hover text-size-16 shadow-bottom-small">
+        <img :src="`components-thumbnails/${componentNameToKebabCase(name)}.jpg`" />
+        <q-card-section class="text-lp-primary text-weight-medium">
+          {{ name }}
         </q-card-section>
-        <q-card-section>
-          <div class="card__text text-lp-dark">{{ text }}</div>
+        <q-card-section class="text-lp-dark">
+          {{ description }}
         </q-card-section>
       </q-card>
     </div>
@@ -37,55 +31,54 @@
 </template>
 
 <script>
-import { defineComponent, ref, watch } from 'vue'
+import { defineComponent, ref, computed } from 'vue'
 import { components } from 'src/assets/landing-page/image-links.js'
 
 const FILTER_CHIPS = [ 'Buttons', 'Inputs', 'Loading', 'Media', 'Navigation', 'Panels & Popups', 'Scroll', 'Tables', 'Others' ]
+
+const componentNameToKebabCase = (componentName) => componentName.replaceAll(' ', '-').toLowerCase()
 
 export default defineComponent({
   name: 'Components',
   setup () {
     const search = ref('')
 
-    watch(search, () => {
+    computed(search, () => {
       // TODO: implement component filtering
     })
 
     return {
-      search: '',
+      search,
       filterChips: FILTER_CHIPS,
-      components
+      components,
+      componentNameToKebabCase
     }
   }
 })
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .components {
   display: grid;
   grid-template-columns: repeat(5, 1fr);
-  grid-gap: 1rem;
+  gap: 16px;
 }
 
-.card {
+.raise-on-hover {
   transition: transform .3s;
 
-  &__text {
-    letter-spacing: 3px;
-  }
-
   &:hover {
-    box-shadow: $lp-box-shadow--large;
-    transform: scale(1.03)
+   box-shadow: $lp-box-shadow--large;
+   transform: scale(1.03)
   }
 }
 
-.input-btn {
-  padding: 0 10px 0px 10px;
+.search-field::after {
+  content: "";
+  position: absolute;
+  width: 108%;
+  left: -4%;
   border-bottom: 1px solid $lp-primary;
-
-  .search-field {
-    color: $white;
-  }
+  top: 40px;
 }
 </style>
